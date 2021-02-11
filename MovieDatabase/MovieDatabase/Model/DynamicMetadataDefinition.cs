9a1c3bbbc77a3cc13fd680cc9e3849c6e3412ba1
paqaos <pawel.labno@outlook.com>
2021-02-11 23:25:54 +0100

    Initial commit

---
+++ MovieDatabase/MovieDatabase/Model/DynamicMetadataDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Model
+{
+    public class DynamicMetadataDefinition : DatabaseItem
+    {
+        /// <inheritdoc />
+        public override string Type { get; } = "DynamicMetadata";
+        public string Name { get; set; }
+
+        public IList<DynamicMetadataProperty> Properties { get; set; } = new List<DynamicMetadataProperty>();
+
+        public List<ValidatorConfiguration> Validators { get; set; } = new List<ValidatorConfiguration>();
+    }
+}
